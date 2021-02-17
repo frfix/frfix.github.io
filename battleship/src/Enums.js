19cee168c86065a3a0fa3f83a2027abfbdeec49c
frfix <mtokchi@gmail.com>
2021-02-18 01:02:16 +0700

    files deleted

--- battleship/src/Enums.js
+++
@@ -1,7 +0,0 @@
-export class Enums{  // Класс с необходимыми перечислениями
-  static CellStatusEnum = {occupied:"occupied", free:"free"};
-  static ShipOrientationEnum = {Horizontal:"Horizontal", Vertical:"Vertical"};
-  static BoardOwnerEnum = {player : "player", ai : "ai"};
-  static CellHitResultEnum = {damaged : "damaged", miss : "miss"};
-  static ShipStatusEnum = {alive : "alive", dead : "dead"};
-}
